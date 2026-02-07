30730d8dde353edf2b2f59cf955c93b664dbf716
Easy Vehicle Usa <col.store2025@gmail.com>
2026-02-07 06:56:49 -0500

    Actualizar firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,10 +3,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "tallerpro360.firebaseapp.com",
-  projectId: "tallerpro360",
+  apiKey: "XXX",
+  authDomain: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXX",
+  appId: "XXX"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
