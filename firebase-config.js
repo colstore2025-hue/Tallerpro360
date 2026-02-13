4f2ef91787a03015c10d544b9efbad8681e34bcc
Easy Vehicle Usa <col.store2025@gmail.com>
2026-02-12 19:14:44 -0500

    Actualizar firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,12 +5,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "PEGA_AQUI_TU_APIKEY_REAL",
-  authDomain: "PEGA_AQUI_TU_AUTHDOMAIN",
-  projectId: "PEGA_AQUI_TU_PROJECTID",
-  storageBucket: "PEGA_AQUI_TU_STORAGE",
-  messagingSenderId: "PEGA_AQUI_TU_SENDERID",
-  appId: "PEGA_AQUI_TU_APPID"
+  apiKey: "AIzaSyAdk-s-OXu57MiobzRGBRu-TlF2KYeicWQ",
+  authDomain: "tallerpro360.firebaseapp.com",
+  projectId: "tallerpro360",
+  storageBucket: "tallerpro360.firebasestorage.app",
+  messagingSenderId: "636224778184",
+  appId: "1:636224778184:web:9bd7351b6458a1ef625afd",
+  measurementId: "G-VEC2C0QX2G"
 };
 
 const app = initializeApp(firebaseConfig);
