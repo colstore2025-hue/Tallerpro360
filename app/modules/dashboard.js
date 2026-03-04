7b141ab3ead95964d58b4a2603957aab4da211de
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-04 11:40:16 -0500

    Crear modules/dashboard.js

---
+++ app/modules/dashboard.js
@@ -0,0 +1,6 @@
+export function dashboard(container) {
+  container.innerHTML = `
+    <h1 class="text-2xl font-bold mb-4">Dashboard Ejecutivo</h1>
+    <p>Bienvenido al núcleo MultiServicios PRO.</p>
+  `;
+}
