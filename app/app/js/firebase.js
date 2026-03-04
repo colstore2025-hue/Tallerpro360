82a72b04e2365f3da61e3fe3dc0f67f04f1c1f6c
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-04 11:55:05 -0500

    Crear app/js/firebase.js

---
+++ app/app/js/firebase.js
@@ -0,0 +1,20 @@
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Config
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "tallerpro360.firebaseapp.com",
+  projectId: "tallerpro360",
+  storageBucket: "tallerpro360.firebasestorage.app",
+  messagingSenderId: "636224778184",
+  appId: "1:636224778184:web:9bd7351b6458a1ef625afd",
+  measurementId: "G-VEC2C0QX2G"
+};
+
+// Init
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
