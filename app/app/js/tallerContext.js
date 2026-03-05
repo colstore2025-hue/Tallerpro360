5ca9796223d8c01ae6c6d443fa074abf849eb346
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-05 08:01:10 -0500

    Actualizar tallerContext.js

--- app/app/js/tallerContext.js
+++ app/app/js/tallerContext.js
@@ -1,5 +1,11 @@
+export function setTallerId(tallerId){
+  localStorage.setItem("tallerId",tallerId);
+}
+
 export function obtenerTallerId(){
+  return localStorage.getItem("tallerId");
+}
 
-return localStorage.getItem("tallerId");
-
+export function limpiarTaller(){
+  localStorage.removeItem("tallerId");
 }
