48eb58f0b42566013c8b6bdb2a1ece39840d02b9
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-05 08:03:03 -0500

    Crear dbTaller.js

---
+++ app/app/js/dbTaller.js
@@ -0,0 +1,19 @@
+import { db } from "./firebase.js";
+import { obtenerTallerId } from "./tallerContext.js";
+
+import {
+collection
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+export function coleccionTaller(nombre){
+
+const tallerId = obtenerTallerId();
+
+return collection(
+db,
+"talleres",
+tallerId,
+nombre
+);
+
+}
