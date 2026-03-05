870c16b6da7fa1621bc50a26744d48b3302e2ac4
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-05 06:55:01 -0500

    Eliminar whatsappService.js

--- app/app/js/whatsappService.js
+++
@@ -1,9 +0,0 @@
-export function enviarWhatsApp(clienteTelefono,mensaje){
-
-const texto = encodeURIComponent(mensaje);
-
-const url = `https://wa.me/57${clienteTelefono}?text=${texto}`;
-
-window.open(url,"_blank");
-
-}
