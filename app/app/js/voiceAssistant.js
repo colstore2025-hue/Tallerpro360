521327b7010bafc0ef2deec50a95991f16ffac5a
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-05 06:04:51 -0500

    Crear voiceAssistant.js

---
+++ app/app/js/voiceAssistant.js
@@ -0,0 +1,32 @@
+export function iniciarVoz(){
+
+const recognition = new webkitSpeechRecognition();
+
+recognition.lang = "es-CO";
+recognition.continuous = false;
+
+recognition.onresult = function(event){
+
+const texto = event.results[0][0].transcript;
+
+console.log("Voz:",texto);
+
+procesarComando(texto);
+
+};
+
+recognition.start();
+
+}
+
+function procesarComando(texto){
+
+texto = texto.toLowerCase();
+
+if(texto.includes("crear orden")){
+
+alert("Comando detectado: crear orden");
+
+}
+
+}
