22ccd043690bc8321bd4dff4d71caada4c0625a3
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-05 06:48:22 -0500

    Actualizar ordenesAcciones.js

--- app/app/js/ordenesAcciones.js
+++ app/app/js/ordenesAcciones.js
@@ -3,29 +3,50 @@ import { db } from "./firebase.js";
 import {
 doc,
 updateDoc,
-arrayUnion
+arrayUnion,
+getDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-export async function agregarAccionOrden(ordenId, accion){
 
-const empresaId = localStorage.getItem("empresaId");
+export async function agregarAccionOrden(ordenId, accion, costo = 0){
 
-const ref = doc(
-db,
-"empresas",
-empresaId,
-"ordenes",
-ordenId
-);
+const ref = doc(db,"ordenes",ordenId);
 
 await updateDoc(ref,{
 acciones: arrayUnion({
 descripcion:accion,
+costo:costo,
 fecha:new Date(),
 estado:"pendiente"
 })
 });
 
-alert("Acción agregada a la orden");
+await recalcularTotal(ordenId);
+
+}
+
+
+
+export async function recalcularTotal(ordenId){
+
+const ref = doc(db,"ordenes",ordenId);
+
+const snap = await getDoc(ref);
+
+const data = snap.data();
+
+let total = 0;
+
+if(data.acciones){
+
+data.acciones.forEach(a=>{
+total += Number(a.costo || 0);
+});
+
+}
+
+await updateDoc(ref,{
+total:total
+});
 
 }
