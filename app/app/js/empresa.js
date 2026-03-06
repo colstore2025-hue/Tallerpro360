7229c4075732cc1c5108a69658db937481c421f2
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-05 21:52:30 -0500

    Actualizar empresa.js

--- app/app/js/empresa.js
+++ app/app/js/empresa.js
@@ -1,16 +1,14 @@
 import { db } from "./firebase.js";
 import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-export async function crearEmpresa(userId, nombreEmpresa){
-
+export async function crearEmpresa(userId, nombreEmpresa) {
   const empresaId = "empresa_" + userId;
 
-  await setDoc(doc(db,"empresas",empresaId),{
-    nombre:nombreEmpresa,
-    plan:"trial",
-    fechaRegistro:new Date()
+  await setDoc(doc(db, "empresas", empresaId), {
+    nombre: nombreEmpresa,
+    plan: "trial",
+    fechaRegistro: new Date()
   });
 
-  localStorage.setItem("empresaId",empresaId);
-
+  localStorage.setItem("empresaId", empresaId);
 }
