625a35fabd53380d6e549cde00d205e3666af249
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-06 08:03:25 -0500

    Crear vehicleScanner.js

---
+++ app/app/js/vehicleScanner.js
@@ -0,0 +1,38 @@
+class VehicleScanner {
+
+constructor(){
+this.vehicleData = {}
+}
+
+scanVehicle(obdData, customerSymptoms){
+
+console.log("🔎 Escaneando vehículo...")
+
+this.vehicleData.obd = obdData
+this.vehicleData.symptoms = customerSymptoms
+
+return this.analyze()
+}
+
+analyze(){
+
+let diagnosis = []
+
+if(this.vehicleData.obd.engineTemp > 105){
+diagnosis.push("Sobrecalentamiento del motor")
+}
+
+if(this.vehicleData.obd.batteryVoltage < 12){
+diagnosis.push("Batería baja o alternador defectuoso")
+}
+
+if(this.vehicleData.symptoms.includes("ruido frenos")){
+diagnosis.push("Posible desgaste de pastillas")
+}
+
+return diagnosis
+}
+
+}
+
+module.exports = VehicleScanner
