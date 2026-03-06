22700d00ba104d31d7580e9687533aa0299e8c98
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-05 21:51:02 -0500

    Actualizar dbTaller.js

--- app/app/js/dbTaller.js
+++ app/app/js/dbTaller.js
@@ -1,19 +1,14 @@
 import { db } from "./firebase.js";
 import { obtenerTallerId } from "./tallerContext.js";
+import { collection } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-import {
-collection
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+export function coleccionTaller(nombre) {
+  const tallerId = obtenerTallerId();
 
-export function coleccionTaller(nombre){
-
-const tallerId = obtenerTallerId();
-
-return collection(
-db,
-"talleres",
-tallerId,
-nombre
-);
-
+  return collection(
+    db,
+    "talleres",
+    tallerId,
+    nombre
+  );
 }
