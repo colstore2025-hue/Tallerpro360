29d6cc97b445b4e823b18aa911816718ad5bc369
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-08 21:02:40 -0500

    Crear inventarioMovimientoService.js

---
+++ app/app/js/services/inventarioMovimientoService.js
@@ -0,0 +1,34 @@
+import { db } from "./firebase.js";
+
+import {
+doc,
+updateDoc,
+increment,
+addDoc,
+collection
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+export async function usarRepuesto(
+ordenId,
+repuestoId,
+cantidad
+){
+
+const ref = doc(db,"repuestos",repuestoId);
+
+await updateDoc(ref,{
+stock:increment(-cantidad)
+});
+
+await addDoc(
+collection(db,"inventario_movimientos"),
+{
+ordenId,
+repuestoId,
+cantidad,
+tipo:"salida",
+fecha:new Date()
+});
+
+}
