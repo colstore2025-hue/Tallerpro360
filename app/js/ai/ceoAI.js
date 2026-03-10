a775f0ceb74ead3817ca73c1685ee954741756bb
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-10 14:58:10 -0500

    Crear ceoAI.js

---
+++ app/js/ai/ceoAI.js
@@ -0,0 +1,11 @@
+// app/js/ai/ceoAI.js
+
+import { analizarRentabilidad } from "./motorIAglobal.js";
+
+export async function analisisCEO(data){
+
+  const analisis = await analizarRentabilidad(data);
+
+  return analisis;
+
+}
