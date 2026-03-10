fa1e5104b851d582a8ff8a17747f38d79b9d6625
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-10 07:06:08 -0500

    Delete app/app/js/core/tallerContext.js

--- app/app/js/core/tallerContext.js
+++
@@ -1,8 +0,0 @@
-// tallerContext.js
-export function setTallerId(tallerId) {
-  localStorage.setItem("tallerId", tallerId);
-}
-
-export function obtenerTallerId() {
-  return localStorage.getItem("tallerId");
-}
