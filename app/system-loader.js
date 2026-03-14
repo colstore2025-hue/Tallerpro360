b01b0eda1e8a48b1ea01bbc5bff1dec3a027604d
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-14 18:40:46 -0500

    Actualizar system-loader.js

--- app/system-loader.js
+++ app/system-loader.js
@@ -1,14 +1,12 @@
 /**
  * system-loader.js
  * Bootloader del ERP
- * TallerPRO360 - Versión Final Integrada
+ * TallerPRO360
  */
 
 import { iniciarApp } from "./js/core/app-init.js";
 
 console.log("⚡ System Loader iniciado");
 
-/* =============================
-   Inicializar la aplicación
-============================= */
+// Inicializa la app principal
 iniciarApp();
