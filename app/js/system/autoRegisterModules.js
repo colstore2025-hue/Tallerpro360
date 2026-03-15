ccb6860dc11f5a81d81c68a17a48a11870f28a04
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-14 22:39:53 -0500

    Crear autoRegisterModules.js

---
+++ app/js/system/autoRegisterModules.js
@@ -0,0 +1,19 @@
+/**
+ * autoRegisterModules.js
+ * Importa todos los módulos del sistema
+ * para auto-registro en moduleLoader
+ */
+
+/* módulos ERP */
+
+import "../modules/dashboard.js";
+import "../modules/clientes.js";
+import "../modules/ordenes.js";
+import "../modules/inventario.js";
+import "../modules/finanzas.js";
+import "../modules/contabilidad.js";
+import "../modules/pagosTaller.js";
+import "../modules/ceo.js";
+import "../modules/aiAssistant.js";
+import "../modules/aiAdvisor.js";
+import "../modules/configuracion.js";
