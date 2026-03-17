a0c4a2d697b753d0de09d9f6f1a2652cc886e589
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-17 07:16:17 -0500

    Actualizar firebase-config.js

--- app/js/core/firebase-config.js
+++ app/js/core/firebase-config.js
@@ -1,16 +1,22 @@
+/**
+ * Firebase Config - Global
+ */
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-      apiKey: "AIzaSyAdk-s-OXu57MiobzRGBRu-TlF2KYeicWQ",
-      authDomain: "tallerpro360.firebaseapp.com",
-      projectId: "tallerpro360",
-      storageBucket: "tallerpro360.firebasestorage.app",
-      messagingSenderId: "636224778184",
-      appId: "1:636224778184:web:9bd7351b6458a1ef625afd",
-      measurementId: "G-VEC2C0QX2G"
-    };
+  apiKey: "AIzaSyAdk-s-OXu57MiobzRGBRu-TlF2KYeicWQ",
+  authDomain: "tallerpro360.firebaseapp.com",
+  projectId: "tallerpro360",
+  storageBucket: "tallerpro360.firebasestorage.app",
+  messagingSenderId: "636224778184",
+  appId: "1:636224778184:web:9bd7351b6458a1ef625afd",
+  measurementId: "G-VEC2C0QX2G"
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-window.db = db; // acceso global
+const db = getFirestore(app);
+
+// 🔥 Global
+window.db = db;
+export { db };
