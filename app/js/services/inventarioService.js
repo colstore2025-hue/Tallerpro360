647b567db1178eb099e9ea0ef9c427acb4288e6c
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-17 07:12:08 -0500

    Actualizar inventarioService.js

--- app/js/services/inventarioService.js
+++ app/js/services/inventarioService.js
@@ -1,43 +1,7 @@
-import {
-  doc,
-  updateDoc,
-  increment,
-  addDoc,
-  collection,
-  getDoc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
 /**
- * 🔻 Descuenta stock cuando se usa en una orden
+ * Inventario básico
  */
-export async function usarRepuesto({ repuestoId, cantidad, ordenId }) {
-
-  const ref = doc(window.db, "repuestos", repuestoId);
-  const snap = await getDoc(ref);
-
-  if (!snap.exists()) {
-    throw new Error("Repuesto no existe");
-  }
-
-  const data = snap.data();
-
-  if (data.stock < cantidad) {
-    throw new Error("Stock insuficiente");
-  }
-
-  // 🔻 Descontar stock
-  await updateDoc(ref, {
-    stock: increment(-cantidad)
-  });
-
-  // 🧾 Registrar movimiento
-  await addDoc(collection(window.db, "movimientosInventario"), {
-    repuestoId,
-    tipo: "salida",
-    cantidad,
-    ordenId,
-    fecha: new Date()
-  });
-
-  return data;
+export async function usarRepuesto({repuestoId,cantidad,ordenId}){
+  console.log("Usando repuesto:", repuestoId,cantidad,ordenId);
+  return true;
 }
