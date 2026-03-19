f4bc67b6e1b7b2f395ee70ee4ebc41f1cad96cec
Easy Vehicle Usa <col.store2025@gmail.com>
2026-03-19 13:53:54 -0500

    Actualizar voiceCore.js

--- app/js/voice/voiceCore.js
+++ app/js/voice/voiceCore.js
@@ -2,9 +2,11 @@ export function hablar(texto) {
   try {
     const speech = new SpeechSynthesisUtterance(texto);
     speech.lang = "es-CO";
+    speech.rate = 1;
+    speech.pitch = 1;
     window.speechSynthesis.speak(speech);
   } catch (e) {
-    console.warn("Error voz:", e);
+    console.warn("Voice error:", e);
   }
 }
 
@@ -12,8 +14,8 @@ export function iniciarVoz(callback) {
   const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
   recognition.lang = "es-CO";
 
-  recognition.onresult = (event) => {
-    const texto = event.results[0][0].transcript;
+  recognition.onresult = e => {
+    const texto = e.results[0][0].transcript;
     callback(texto);
   };
 
