e5ebe59cd82fb7f68aa137358079add2293df20c
Easy Vehicle Usa <col.store2025@gmail.com>
2026-04-15 11:59:18 -0500

    Actualizar contabilidad.js

--- app/js/modules/contabilidad.js
+++ app/js/modules/contabilidad.js
@@ -8,7 +8,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"; 
 import { db } from "../core/firebase-config.js";
 
-
 export default async function contabilidad(container) {
     const empresaId = localStorage.getItem("nexus_empresaId") || localStorage.getItem("empresaId");
     let vistaActual = "DIARIO"; 
