818675fe067104f054c71eefb5adaf3ce37ac935
Easy Vehicle Usa <col.store2025@gmail.com>
2026-04-15 11:57:18 -0500

    Actualizar config.js

--- app/js/modules/config.js
+++ app/js/modules/config.js
@@ -8,10 +8,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { db } from "../core/firebase-config.js";
 import { hablar } from "../voice/voiceCore.js";
-import { initNexusHelp } from './modules/nexus-help-engine.js';
-
-// Cuando cargue el documento o la vista:
-initNexusHelp('ordenes'); // Le pasas el ID del módulo actual
 
 
 export default async function configModule(container, state) {
